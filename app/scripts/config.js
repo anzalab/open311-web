dc0e6351b6864b709ec168aa06bdf262572ec9d6
lykmapipo <lallyelias87@gmail.com>
2017-11-17 15:34:36 +0300

    commit all local changes

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
 angular.module('ng311')
 
-.constant('ENV', {name:'production',owner:'DAWASCO',title:'SmartDawasco',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{mobile:'',web:''},socketEndPoint:{mobile:'',web:''},socketEnable:false,settings:{locale:'sw',name:'open311',email:'example@example.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
+.constant('ENV', {name:'development',owner:'DAWASCO',title:'SmartDawasco',version:'v0.1.0',description:'Citizen Feedback System',apiEndPoint:{web:'http://127.0.0.1:3000',mobile:'http://127.0.0.1:3000'},socketEndPoint:{web:'http://127.0.0.1:3000',mobile:'http://127.0.0.1:3000'},socketEnable:false,settings:{locale:'sw',name:'open311',email:'example@example.com',phone:'(000) 000 000 000',currency:'USD',dateFormat:'dd/MM/yyyy',timeFormat:'hh:mm:ss',defaultPassword:'guest',abbreviations:{thousand:'K',million:'M',billion:'B',trillion:'T'}}})
 
 ;
