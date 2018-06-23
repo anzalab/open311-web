fe0b3b53c1ed112b3fbe370e5ffc7b2101c5d758
Benson Maruchu <benmaruchu@gmail.com>
2018-06-23 13:42:10 +0300

    Add filter modal window for profile report

--- app/scripts/controllers/auth/profile.js
+++ app/scripts/controllers/auth/profile.js
@@ -10,7 +10,7 @@
 angular
   .module('ng311')
   .controller('AuthProfileCtrl', function (
-    $rootScope, $scope, $state, $auth, Party
+    $rootScope, $scope, $state, $auth, $uibModal, Party
   ) {
 
     //signal if its editing process
@@ -103,13 +103,13 @@ angular
       //TODO show input prompt
       //TODO show loading mask
       $scope.party.$update().then(function (response) {
-          if (passwordChanged) {
-            //signout current party
-            return $auth.signout();
-          } else {
-            return response;
-          }
-        })
+        if (passwordChanged) {
+          //signout current party
+          return $auth.signout();
+        } else {
+          return response;
+        }
+      })
         .then(function (response) {
           response = response || {};
 
@@ -142,19 +142,49 @@ angular
         //TODO comment
         response.pipelines =
           _.chain(response.pipelines)
-          .orderBy('label.weight', 'asc')
-          .map(function (pipeline) {
-            return _.merge({}, {
-              displayColor: _.get(pipeline, 'label.color',
-                '#4BC0C0') + '!important'
-            }, pipeline);
-          }).value();
+            .orderBy('label.weight', 'asc')
+            .map(function (pipeline) {
+              return _.merge({}, {
+                displayColor: _.get(pipeline, 'label.color',
+                  '#4BC0C0') + '!important'
+              }, pipeline);
+            }).value();
 
         response.leaderboard =
           _.orderBy(response.leaderboard, 'count', 'desc');
         console.log(response);
         $scope.performances = response;
       });
+    };
+
+
+    /**
+     * Open date filters for profile reports
+     */
+    $scope.showFilter = function () {
+
+      //open overview reports filter modal
+      $scope.modal = $uibModal.open({
+        templateUrl: 'views/auth/_partials/profile_filter.html',
+        scope: $scope,
+        size: 'lg',
+      });
+
+      //handle modal close and dismissed
+      $scope.modal.result.then(function onClose( /*selectedItem*/) { },
+        function onDismissed() { });
+
+    };
+
+    /**
+     * Filter profile reports based on on current selected filters
+     * @param {Boolean} [reset] whether to clear and reset filter
+     */
+    $scope.filter = function (reset) {
+
+      if (reset) {
+        //TODO clear filters
+      }
     };
 
     $scope.performance();
